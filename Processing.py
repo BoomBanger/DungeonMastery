b47de0d02a96288bdbcc1ca4172a84997276e768
MedievalWeapon <lancefowler22@gmail.com>
2022-04-02 19:35:08 -0400

    new stuff

--- Processing.py
+++ Processing.py
@@ -2,6 +2,7 @@ from random import random
 import numpy as np
 import cv2
 
+#gets the values around the border of an array
 def getBorder(array):
     border = []
     border += list(array[0, :-1])
@@ -10,7 +11,7 @@ def getBorder(array):
     border +=list(array[::-1, 0])
     return np.array(border)
     
-
+#gets largest blob in image, if noblobswithlotsofedges is true, prioritizes blobs that don't touch the edge often
 def blobWithMaxArea(mask, noblobswithlotsofedges=False):
     numLabelsOut, labelsOut, statsOut, _ = cv2.connectedComponentsWithStats(mask, 8)
     sizes = statsOut[:, -1]
@@ -27,6 +28,7 @@ def blobWithMaxArea(mask, noblobswithlotsofedges=False):
 
     return np.uint8(label)
 
+#removes blobs in image smaller than {area}
 def removeSmallBlobs(arr, area):
     numLabelsOut, labelsOut, statsOut, _ = cv2.connectedComponentsWithStats(arr, 8)
     sizes = statsOut[:, -1]
@@ -39,31 +41,26 @@ def removeSmallBlobs(arr, area):
 
     return img
 
-def removeSmallBlobs(arr, area):
-    numLabelsOut, labelsOut, statsOut, _ = cv2.connectedComponentsWithStats(arr, 8)
-    sizes = statsOut[:, -1]
-    min_size = area
-    img = np.zeros(labelsOut.shape, dtype="uint8")
-    
-    for i in range(1,numLabelsOut):
-        if(sizes[i] > min_size):
-            img[labelsOut == i] = 255
-
-    return img
-
+#if pixels are less than half bright, makes it black, if more than half bright, makes it white
 def binary(arr):
     return cv2.inRange(arr, 128, 255)
 
-def scale(arr, upscale):
-    w = int(arr.shape[1]*upscale)
-    h = int(arr.shape[0]*upscale)
-    return cv2.resize(arr, (w, h), interpolation=cv2.INTER_NEAREST)
+#scales image to have a width of {width} pixels
+def scale(arr, width):
+    w = arr.shape[1]
+    h = arr.shape[0]
+    h, w = int(h * width/w), width
+    img = cv2.resize(arr, (w, h), interpolation=cv2.INTER_NEAREST)
+    cv2.imwrite("downscale.png", img)
+    return img
 
+#smooths walls
 def smooth(arr, smoothingSize:int):
     kernel = np.ones((smoothingSize, smoothingSize)) / (smoothingSize * smoothingSize)
     arr = cv2.filter2D(arr, -1, kernel)
     return arr
 
+#does expansion of walls as part of wavyness
 def expand(array, size:int):
     arr = array[:,:]
     expanded = cv2.bitwise_not(array)
@@ -77,6 +74,7 @@ def expand(array, size:int):
     arr = cv2.bitwise_not(arr)
     return arr
 
+#does contraction of walls as part of wavyness
 def contract(array, size:int):
     arr = array[:,:]
     contracted = array
@@ -88,31 +86,54 @@ def contract(array, size:int):
     arr = cv2.dilate(arr, np.ones((size,size)))
     return arr
 
+#does random wavyness roughening
 def randomize(arr, size:int):
     arr = contract(arr, size)
     arr = expand(arr, size)
     return arr
 
+#does wavyness stuff on multiple scales with {wavyness} numbe rof operations
 def waves(arr, wavyness:int, smoothing:int):
     for w in range(wavyness, 1, -1):
         val = 2*w + 1
         arr = randomize(arr, val)
-        arr = smooth(arr, int(smoothing / (2 * w)) * 2 + 1)
-        arr = binary(arr)
-        print(w)
     return arr
 
-def process(array, upscale, smoothing, wavyness):
+#draws grid on {arr} with a spacing of {width} pixels
+def drawGrid(arr, width:int):
+    h, w = arr.shape
+    arr = cv2.cvtColor(arr, cv2.COLOR_GRAY2BGR)
+    for x in range(0, w, width):
+        cv2.line(arr, (x, 0), (x, h), (0, 255, 0))
+    for y in range(0, h, width):
+        cv2.line(arr, (0, y), (w, y), (0, 255, 0))
+    return arr
+
+#process image and save it in img.png
+#upscale: width of the target image
+#smoothing: how round you want it to be
+#wavyness: how bumpy you want it to be
+#gridWidth: pixel width between gridlines. 0 is no grid
+def process(array, upscale, smoothing, wavyness, gridWidth):
+    smoothing = smoothing * 2 + 1
+    wavyness = wavyness * 2 + 1
     array = np.float32(array) * 255
     array = scale(array, upscale)
     array = binary(array)
     array = waves(array, wavyness, smoothing)
+    array = smooth(array, smoothing)
+    if gridWidth:
+        array = drawGrid(array, gridWidth)
+    array = binary(array)
     cv2.imwrite("img.png", array)
+    return array
 
+#crop all bordering black pixels out of {blob}
 def cropBlob(blob):
     x, y, w, h = cv2.boundingRect(cv2.bitwise_not(blob))
     return blob[y:y+h, x:x+w]
 
+#get wall array of 1s and 0s from image located at {file}
 def fromImage(file):
     img = cv2.imread(file)
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -133,16 +154,15 @@ def fromImage(file):
     
     
 if __name__ == "__main__":
-    """arr =   [[0, 1, 0, 0, 0, 0, 1, 1, 0, 0],
+    arr =   [[0, 1, 0, 0, 0, 0, 1, 1, 0, 0],
             [0, 1, 1, 1, 0, 0, 1, 1, 0, 0],
             [0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
             [0, 1, 1, 1, 0, 0, 1, 0, 0, 0],
             [0, 0, 1, 0, 0, 1, 1, 1, 0, 0],
             [0, 0, 1, 1, 1, 1, 0, 1, 1, 1],
             [0, 0, 1, 0, 0, 1, 1, 1, 0, 0],
-            [1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
-            [1, 1, 1, 0, 0, 1, 1, 1, 1, 1]]
-    cv2.imshow("img", process(arr, 100, 10, 15))"""
-    cv2.imshow("img.jpg", fromImage("Dungeon7.jpg"))
+            [1, 1, 1, 0, 0, 0, 1, 0, 0, 0]]
+    arr = expand(arr, 20, 20, 20, 20)
+    cv2.imshow("img", process(arr, 1000, 0, 0, 0))
+    #cv2.imshow("img.jpg", fromImage("Dungeon7.jpg"))
     cv2.waitKey(0)
