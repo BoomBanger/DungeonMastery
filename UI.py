c4523e72e9b5f3b25a46a082c3b3a52dbe6cd4e9
notverywise <nickyboyindawindow@gmail.com>
2022-04-02 17:31:55 -0400

    PhotoProcessingUI

--- UI.py
+++ UI.py
@@ -1,7 +1,10 @@
+import tkinter.filedialog
+
 import Processing
-import copy
 import numpy as np
+import cv2
 from tkinter import *
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("BoomBanger Dungeon Generator")
@@ -27,15 +30,81 @@ def getXY():
     global wallArray
 
     buttonArray, wallArray = createMap(xBut.get(), yBut.get())
-    # these destroy the startup screen buttons, allowing for map to take their place
-    xLabel.destroy()
-    yLabel.destroy()
-    xBut.destroy()
-    yBut.destroy()
-    enter.destroy()
+
+    refineMap.grid(row=0, column=yBut.get()+1)
+    # these removes the startup screen buttons, allowing for map to take their place
+    xLabel.grid_forget()
+    yLabel.grid_forget()
+    xBut.grid_forget()
+    yBut.grid_forget()
+    enter.grid_forget()
+    enterPic.grid_forget()
+
 
 enter = Button(root, text="Generate Blank Dungeon Map", command=getXY)
 enter.grid(row=2, column=0, columnspan=2)
+
+
+
+# this section is the photo processing section
+def runPhotoProcess():
+
+    imgPath = imgLabel.cget('text')
+    imgPath = imgPath[24:]
+    print(imgPath)
+    # Processing.fromImage(imgPath, upscale, smoothing, wavyness)
+
+# brings user to the image upload/selection screen
+def picScreen():
+    # removes unneeded widgets
+    xLabel.grid_forget()
+    yLabel.grid_forget()
+    xBut.grid_forget()
+    yBut.grid_forget()
+    enter.grid_forget()
+    enterPic.grid_forget()
+
+    instructions.grid(row=0, column=0)
+    selectBut.grid(row=1, column=0)
+    imgLabel.grid(row=2, column=0)
+
+
+# runs all the update screen information once a user selects a photo
+def getImage():
+    picId = tkinter.filedialog.askopenfilename()
+    imgLabel.config(text='Your image location is: ' + picId)
+    img = Image.open(picId)
+
+    tkimage = ImageTk.PhotoImage(img)
+    aspectRatio = tkimage.height()/tkimage.width()
+
+    resized_image = img.resize((300, int(300 * aspectRatio)), Image.Resampling.LANCZOS)
+    new_image = ImageTk.PhotoImage(resized_image)
+    picLabel.image = new_image
+    picLabel.config(image=new_image)
+    picLabel.grid(row=3, column=0)
+    contin.grid(row=4, column=0)
+
+
+# all of these widgets are used/related to the picture dungeon creation tool
+instructions = Label(root, text='Please select an image using the button below that you would like to convert into '
+                                'a dungeon. Make sure the image has clear lines and is drawn on a white background '
+                                'with either a pencil or other dark colored utensil.')
+enterPic = Button(root, text="Generate Dungeon Using Picture", command=picScreen)
+enterPic.grid(row=3, column=0, columnspan=2)
+selectBut = Button(root, text="Select Image", command=getImage)
+imgLabel = Label(root, text="Your image location is: ")
+picLabel = Label(root)
+contin = Button(root, text="Submit Photo", command=runPhotoProcess)
+
+
+# runs the image generation on the user created dungeon layout
+def runProcess():
+    # parameters are wallArray, upscale: min-25, smoothness: min-0, wavyness: min-0)
+    cv2.imshow("img", Processing.process(wallArray, 25, 0, 0))
+
+
+refineMap = Button(root, text="Process User-Drawn Map", height=2, width=20, command=runProcess)
 
 
 # creates dungeon and gives 2d array of buttons and their color values
@@ -83,3 +152,4 @@ def makeWalls(x, y):
 
 print("HI")
 root.mainloop()
+# roadmap of program looks like
